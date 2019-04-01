3e8671b474eec29b0ae42962c635c60a6e9fa3e4
shenxiaojian <sjpn741@163.com>
2019-04-01 16:17:57 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,16 @@
+export default {
+    singular: true,  //单页应用
+    plugins:[
+        [
+            'umi-plugin-react',{
+                antd:true,
+                dva:true
+            }
+        ]
+    ],
+    routes:[
+        {
+            
+        }
+    ]
+}
